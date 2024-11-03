63322ea75e6691182cb32bb0cf7a2bc8392635ac
Emmanuel-Manny-Web <ipaliboha@gmail.com>
2024-11-03 17:41:50 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import { useEffect, useState } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
 
-import Loader from './common/Loader';
-import PageTitle from './components/PageTitle';
-import SignIn from './pages/Authentication/SignIn';
-import SignUp from './pages/Authentication/SignUp';
-import Calendar from './pages/Calendar';
-import Chart from './pages/Chart';
-import ECommerce from './pages/Dashboard/ECommerce';
-import FormElements from './pages/Form/FormElements';
-import FormLayout from './pages/Form/FormLayout';
-import Profile from './pages/Profile';
-import Settings from './pages/Settings';
-import Tables from './pages/Tables';
-import Alerts from './pages/UiElements/Alerts';
-import Buttons from './pages/UiElements/Buttons';
-import DefaultLayout from './layout/DefaultLayout';
+import Loader from './admin/common/Loader';
+import PageTitle from './admin/components/PageTitle';
+import SignIn from './admin/pages/Authentication/SignIn';
+import SignUp from './admin/pages/Authentication/SignUp';
+import Calendar from './admin/pages/Calendar';
+import Chart from './admin/pages/Chart';
+import ECommerce from './admin/pages/Dashboard/ECommerce';
+import FormElements from './admin/pages/Form/FormElements';
+import FormLayout from './admin/pages/Form/FormLayout';
+import Profile from './admin/pages/Profile';
+import Settings from './admin/pages/Settings';
+import Tables from './admin/pages/Tables';
+import Alerts from './admin/pages/UiElements/Alerts';
+import Buttons from './admin/pages/UiElements/Buttons';
+import DefaultLayout from './admin/layout/DefaultLayout';
 
 function App() {
   const [loading, setLoading] = useState<boolean>(true);
